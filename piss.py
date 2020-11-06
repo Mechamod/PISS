f0578546f4fd79bd101b4864c2e8fa57200f2552
Tim Anders <timanders.95@hotmail.de>
2020-11-06 21:33:12 +0100

    Implemented directory browsing

--- piss.py
+++ piss.py
@@ -3,41 +3,85 @@ from PyQt5.QtWidgets import *
 class PISS:
 
     # Variables
-    WIDTH = 1920
-    HEIGHT = 1080
+    MINIMUM_HEIGHT = 600
+    MINIMUM_WIDTH = 800
+    MAXIMUM_WIDTH = 1920
+    MAXIMUM_HEIGHT = 1080
     INPUT_PATH = ''
     OUTPUT_PATH = ''
+
+    # Components (not neccessary, but good to have a look)
     VIEWER = None
     APP = None
+    LAYOUT = None
+    btn_set_input_path = None
+    btn_set_output_path = None
+    lbl_input_path = None
+    lbl_output_path = None
 
     def __init__(self, width=800, height=600):
         self.WIDTH = width
         self.HEIGHT = height
 
     def create_main_window(self):
+
+        # Define window
         self.APP = QApplication([])
-        self.VIEWER = QWidget()
+        self.VIEWER = QWidget(minimumHeight=self.MINIMUM_HEIGHT, minimumWidth=self.MINIMUM_WIDTH, maximumHeight=self.MAXIMUM_HEIGHT, maximumWidth=self.MAXIMUM_WIDTH)
         self.VIEWER.setWindowTitle("Python Image Selection Saver")
-        #
-        #
-        # STUFF
+        self.LAYOUT = QVBoxLayout()
+
+        # Decorate window
+        ## Labels
         self.create_label_input_path()
-        #
+        self.create_label_output_path()
+
+        ## Buttons
+        self.create_set_input_path_button()
+        self.create_set_output_path_button()
+
+        # Start app
+        self.VIEWER.setLayout(self.LAYOUT)
         self.VIEWER.show()
         self.APP.exec_()
 
-    def create_set_path_button(self):
-        btn_set_path = Button(self.VIEWER, text="Set up input directory", width=25, callback=set_input_path)
+    def create_set_input_path_button(self):
+        self.btn_set_input_path = QPushButton(self.VIEWER)
+        self.btn_set_input_path.setText("Select input folder")
+        self.btn_set_input_path.clicked.connect(self.set_input_path)
+        self.LAYOUT.addWidget(self.btn_set_input_path)
+
+    def create_set_output_path_button(self):
+        self.btn_set_output_path = QPushButton(self.VIEWER)
+        self.btn_set_output_path.setText("Select output folder")
+        self.btn_set_output_path.clicked.connect(self.set_output_path)
+        self.LAYOUT.addWidget(self.btn_set_output_path)
 
     def create_label_input_path(self):
-        lbl_input_path = QLabel(self.VIEWER)
-        lbl_input_path.setText("Input-Path: " + self.INPUT_PATH)
+        self.lbl_input_path = QLabel(self.VIEWER)
+        self.lbl_input_path.setText("Input-Path: " + self.INPUT_PATH)
+        self.LAYOUT.addWidget(self.lbl_input_path)
 
     def create_label_output_path(self):
-        lbl_output_path = QLabel(self.VIEWER, text="Output-Path: " + self.OUTPUT_PATH)
+        self.lbl_output_path = QLabel(self.VIEWER)
+        self.lbl_output_path.setText("Output-Path: " + self.OUTPUT_PATH)
+        self.LAYOUT.addWidget(self.lbl_output_path)
+
+    def update_label_input_path(self):
+        self.lbl_input_path.setText("Input-Path: " + self.INPUT_PATH)
+
+    def update_label_output_path(self):
+        self.lbl_output_path.setText("Output-Path: " + self.OUTPUT_PATH)
 
     def set_input_path(self):
-        self.INPUT_PATH = "Test"
+        self.INPUT_PATH = QFileDialog().getExistingDirectory(None, "Select Input Folder")
+        self.update_label_input_path()
+        print(f"INPUT_PATH set to {self.INPUT_PATH}")
+
+    def set_output_path(self):
+        self.OUTPUT_PATH = QFileDialog().getExistingDirectory(None, "Select Output Folder")
+        self.update_label_output_path()
+        print(f"OUTPUT_PATH set to {self.OUTPUT_PATH}")
 
 if __name__ == '__main__':
     print("Start PISS")
