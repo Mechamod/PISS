16eaf740c9e74e0d36e40b2f2f9c8f41fc09a79d
Tim Anders <timanders.95@hotmail.de>
2020-11-06 21:55:34 +0100

    Created documentation till now

--- piss.py
+++ piss.py
@@ -1,8 +1,14 @@
 from PyQt5.QtWidgets import *
 
 class PISS:
+    """
+    Python Image Selection Saver is a PyQt GUI based program which lets the user
+    load up a collection of images from a folder, providing a preview of the
+    images, an easy way to browse them (left/right arrow buttons) and finally
+    let him save the images he likes with a simple press of the spacebar.
+    """
 
-    # Variables
+    # Variables (Numbers, Strings, Lists)
     MINIMUM_HEIGHT = 600
     MINIMUM_WIDTH = 800
     MAXIMUM_WIDTH = 1920
@@ -19,11 +25,20 @@ class PISS:
     lbl_input_path = None
     lbl_output_path = None
 
-    def __init__(self, width=800, height=600):
-        self.WIDTH = width
-        self.HEIGHT = height
+    def __init__(self):
+        """
+        Init the PISS variables needed beforehand
+        """
+
+        pass
 
     def create_main_window(self):
+        """
+        Creates the main window.
+        First it defines the window and it's properties, then it creates all
+        the elements and forms it has to display and finally
+        it adds everything together.
+        """
 
         # Define window
         self.APP = QApplication([])
@@ -43,47 +58,93 @@ class PISS:
         # Start app
         self.VIEWER.setLayout(self.LAYOUT)
         self.VIEWER.show()
+
+    def start_app(self):
+        """
+        Starts the execution of the application.
+        """
+
         self.APP.exec_()
 
     def create_set_input_path_button(self):
+        """
+        Creates a button which opens a dialog to let the user select the
+        input folder. It also gets added to the layout of the application
+        window.
+        """
+
         self.btn_set_input_path = QPushButton(self.VIEWER)
         self.btn_set_input_path.setText("Select input folder")
         self.btn_set_input_path.clicked.connect(self.set_input_path)
         self.LAYOUT.addWidget(self.btn_set_input_path)
 
     def create_set_output_path_button(self):
+        """
+        Creates a button which opens a dialog to let the user select the
+        output folder. It also gets added to the layout of the application
+        window.
+        """
+
         self.btn_set_output_path = QPushButton(self.VIEWER)
         self.btn_set_output_path.setText("Select output folder")
         self.btn_set_output_path.clicked.connect(self.set_output_path)
         self.LAYOUT.addWidget(self.btn_set_output_path)
 
     def create_label_input_path(self):
+        """
+        Creates a text label that displays the currently set path of the
+        input directory.
+        """
+
         self.lbl_input_path = QLabel(self.VIEWER)
         self.lbl_input_path.setText("Input-Path: " + self.INPUT_PATH)
         self.LAYOUT.addWidget(self.lbl_input_path)
 
-    def create_label_output_path(self):
+    def create_label_output_path(self):#
+        """
+        Creates a text label that displays the currently set path of the
+        output directory.
+        """
+
         self.lbl_output_path = QLabel(self.VIEWER)
         self.lbl_output_path.setText("Output-Path: " + self.OUTPUT_PATH)
         self.LAYOUT.addWidget(self.lbl_output_path)
 
     def update_label_input_path(self):
+        """
+        Updates the label of the input path.
+        """
+
         self.lbl_input_path.setText("Input-Path: " + self.INPUT_PATH)
 
     def update_label_output_path(self):
+        """
+        Updates the label of the output path.
+        """
+
         self.lbl_output_path.setText("Output-Path: " + self.OUTPUT_PATH)
 
     def set_input_path(self):
+        """
+        Opens a dialog form that lets the user choose the input directory.
+        """
+
         self.INPUT_PATH = QFileDialog().getExistingDirectory(None, "Select Input Folder")
         self.update_label_input_path()
         print(f"INPUT_PATH set to {self.INPUT_PATH}")
 
     def set_output_path(self):
+        """
+        Opens a dialog form that lets the user choose the output directory.
+        """
+
         self.OUTPUT_PATH = QFileDialog().getExistingDirectory(None, "Select Output Folder")
         self.update_label_output_path()
         print(f"OUTPUT_PATH set to {self.OUTPUT_PATH}")
 
+# Starts the program!
 if __name__ == '__main__':
     print("Start PISS")
-    piss = PISS(width=1920, height=1080)
+    piss = PISS()
     piss.create_main_window()
+    pss.start_app()
